e164773d3e53eef5ecc3bd38c51832678d669065
Shade <61341723+shadeoxide@users.noreply.github.com>
2020-09-13 10:19:56 +0530

    Bot's Constructor

---
+++ Bot.js
@@ -0,0 +1,24 @@
+const { Client, Collection } = require("discord.js");
+
+class Bot extends Client {
+  /**
+   * @constructor
+   * @param {Object} options Client Options
+   */
+  constructor(options) {
+    super(options);
+    this.db = require('quick.db');
+  }
+}
+
+// error handlers
+process.on("uncaughtException", err => {
+  console.error("Uncaught Exception: ", err);
+  process.exit(1);
+});
+
+process.on("unhandledRejection", err => {
+  console.error("Uncaught Promise Error: ", err);
+});
+
+module.exports = Bot;
