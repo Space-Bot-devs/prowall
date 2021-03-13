9c9b42013d147f9bb6ff81806fdb5db93f87b118
Jonfirexbox <66625200+Jonfirexbox@users.noreply.github.com>
2021-03-13 13:00:36 -0500

    Update about.js

--- commands/about.js
+++ commands/about.js
@@ -5,12 +5,15 @@ module.exports.run = async (bot, message, args) => {
 
   const em = new Discord.MessageEmbed()
     .setTitle('About/Credits')
-    .addField('**Bot Developers:**\n```Kyro#3400, Shade#0003```')
-    .setField(`**Guild Count:**\n${bot.guilds.cache.size} servers`)
+    .addFields(
+		{ name: '**Bot Developers:**', value: '```Kyro#3400, Shade#0003```' },
+		{ name: '**Guild Count:**', value: `${bot.guilds.cache.size} servers`, inline: false },
+		{ name: '**Invite Me**:', value: '[Click Here](https://discord.com/api/oauth2/authorize?client_id=785673868567248916&permissions=2994733046&scope=bot)', inline: true },
+    { name: '**Support Server**:', value: '[Click Here](https://discord.gg/zSazjPu4QQ)', inline: true },
+	)
     .setFooter('Thanks to all contributors for contributing in this project!')
     .setColor("BLURPLE");
   message.channel.send(em)
-
 }
 
 
@@ -19,5 +22,5 @@ module.exports.config = {
   description: "Shows a list of contributors",
   usage: "about",
   accessableby: "Members",
-  aliases: ["contributors"]
+  aliases: ["contributors", "invite"]
 }
