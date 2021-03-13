0d631a5bd644d9c3dcd85ba08c979fb1e3eb8998
Jonfirexbox <66625200+Jonfirexbox@users.noreply.github.com>
2021-03-13 12:07:57 -0500

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -1,5 +1,5 @@
 const prefix = 'sb!'
-const owner = ["560484031838552064", "662207542486630401"];
+const owner = ["680506811031355413", "560484031838552064"];
 module.exports.run = async (client, message, args) => {
   const Discord = require('discord.js')
   if (!owner.includes(message.author.id)) return;
@@ -7,17 +7,17 @@ module.exports.run = async (client, message, args) => {
   let cmdUsage = client.commands.get('eval', 'help.usage');
 
   try {
-    const code = args.join(' ');
-    if (!code) return message.channel.send(`You want to eval air?`).then(msg => msg.delete(3000)).catch(err => client.logger.error(err));
-    let evaled = eval(code);
-
-    if (typeof evaled !== 'string') evaled = require('util').inspect(evaled);
-
-    await message.channel.send(clean(evaled).replace(process.env.TOKEN, "no lmao"), { code: 'xl' });
-  } catch (err) {
-    await message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+      const code = args.join(" ");
+      let evaled = eval(code);
+ 
+      if (typeof evaled !== "string")
+        evaled = require("util").inspect(evaled);
+ 
+      message.channel.send(clean(evaled), {code:"xl"});
+    } catch (err) {
+      message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+    }
   }
-}
 
 module.exports.config = {
   name: "eval",
@@ -31,3 +31,4 @@ const clean = text => {
   if (typeof (text) === 'string') return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
   else return text;
 };
+
