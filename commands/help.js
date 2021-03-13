988b610f2bcba37c3c0cd130ee790204ff40d916
Jonfirexbox <66625200+Jonfirexbox@users.noreply.github.com>
2021-03-13 12:08:51 -0500

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,24 +3,24 @@ const help = require('../help.json')
 
 module.exports.run = async (bot, message, args) => {
   let cmds = message.content.split(' ')[1]
-  if(!cmds){
+  if (!cmds) {
     const embed = new Discord.MessageEmbed()
-    .setTitle('Help Commands')
-    .addField('General', '`ping`, `help`, `snipe`, `editsnipe`, `credits`')
-    .addField('Moderation', '`kick <@user||userID>`, `ban <@user||userID>`, `unban <userID>`')
-    .addField('Config', '`logs <#channel>`, `prefix <prefix>`')
-    .setColor('BLURPLE')
+      .setTitle('Help Commands')
+      .addField('General', '`ping`, `help`, `snipe`, `editsnipe`, `about`')
+      .addField('Moderation', '`kick <@user||userID>`, `ban <@user||userID>`, `unban <userID>`')
+      .addField('Config', '`logs <#channel>`, `prefix <prefix>`')
+      .setColor('BLURPLE')
     message.channel.send(embed)
-  }else{
+  } else {
     var content = null
-    for(let i = 0; i < help.length; i++){
-      if(cmds.toLowerCase() == help[i].name){
+    for (let i = 0; i < help.length; i++) {
+      if (cmds.toLowerCase() == help[i].name) {
         var content = `**Description:**\n${help[i].description}\n**Usage:**\n${help[i].usage}`
       }
     }
     const embed = new Discord.MessageEmbed()
-    .setColor('BLURPLE')
-    if(content == null) embed.setDescription(`Cannot find command ${cmds}`)
+      .setColor('BLURPLE')
+    if (content == null) embed.setDescription(`Cannot find command ${cmds}`)
     else embed.setTitle(`Command: ${cmds}`).setDescription(content)
     message.channel.send(embed)
   }
@@ -28,9 +28,9 @@ module.exports.run = async (bot, message, args) => {
 
 
 module.exports.config = {
-    name: "help",
-    description: "Help Command",
-    usage: "help",
-    accessableby: "Members",
-    aliases: ["cmds"]
+  name: "help",
+  description: "Help Command",
+  usage: "help",
+  accessableby: "Members",
+  aliases: ["cmds"]
 }
